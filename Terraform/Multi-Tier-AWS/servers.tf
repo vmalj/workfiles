ff760d2520a4032960bca1c8a9c547107707cb67
vimtp <vimtp@tp.com>
2020-07-29 18:31:40 +0530

    Removal

--- Terraform/Multi-Tier-AWS/servers.tf
+++
@@ -1,8 +0,0 @@
-## Creating 2 Instances
-resource "aws_instance" "application" {
-    count = 2
-    ami = "ami-a6b8e7ce"
-    instance_type="t2.micro"  
-    subnet_id=aws_subnet.public.id
-    security_groups=[aws_security_group.default.id]
-}
