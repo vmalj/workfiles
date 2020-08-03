223d38baf689797110c74b8dcfc66f47a71bba09
vimtp <vimtp@tp.com>
2020-08-03 19:52:50 +0530

    Started Python

---
+++ python/first1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+first_name = "Vimal Prakash"
+last_name = "J"
+age = 10
+birth_date = "29 August"
+
+print(f"My name is {first_name} {last_name}.")
+print(f"I was born on {birth_date} and my age is {age}.")
