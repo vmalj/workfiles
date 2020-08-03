b04fab606cadf4386f330442399934fd75fe4870
vimtp <vimtp@tp.com>
2020-08-03 22:24:08 +0530

    Added comments

--- python/first1.py
+++ python/first1.py
@@ -6,3 +6,6 @@ birth_date = "29 August"
 
 print(f"My name is {first_name} {last_name}.")
 print(f"I was born on {birth_date} and my age is {age}.")
+
+# Note: The magic of f-strings is that you can embed Python expressions directly inside them.
+# Any portion of an f-string that’s enclosed in curly braces ({}) is treated as an expression.
